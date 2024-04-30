ba1620de5a183b520343329f409f2c87c0a462b9
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-04-30 09:01:18 +0700

    use secoureStorage

--- lib/cache_module/logics/cache_theme_logic.dart
+++ lib/cache_module/logics/cache_theme_logic.dart
@@ -9,41 +9,26 @@ class CacheThemeLogic extends ChangeNotifier {
   FlutterSecureStorage _secure = FlutterSecureStorage();
   final _key = "CacheFlutterSecureStorage";
 
-  // final key = "CacheThemeLogic";
-  // late SharedPreferences _pref;
-
   Future initCahe() async {
-    // _pref = await SharedPreferences.getInstance();
-    // _modeIndex = _pref.getInt(key) ??  1;
     String index = await _secure.read(key: _key) ?? "1";
     _modeIndex = int.parse(index);
     notifyListeners();
   }
 
   void changeToLight() async {
-    // _modeIndex = 1;
-    // _pref.setInt(key,  _modeIndex);
-    // notifyListeners();
-
     _modeIndex = 1;
     _secure.write(key: _key, value: _modeIndex.toString());
     notifyListeners();
   }
 
   void changeToDark() async {
-    // _modeIndex = 2;
-    // _pref.setInt(key,  _modeIndex);
-    // notifyListeners();
-
-        _modeIndex = 2;
+    _modeIndex = 2;
     _secure.write(key: _key, value: _modeIndex.toString());
     notifyListeners();
   }
+
   void changeToSystem() async {
-    // _modeIndex = 3;
-    // _pref.setInt(key,  _modeIndex);
-    // notifyListeners();
-        _modeIndex = 3;
+    _modeIndex = 3;
     _secure.write(key: _key, value: _modeIndex.toString());
     notifyListeners();
   }
